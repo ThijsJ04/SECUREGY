5c4363e088b833872b9621aa27bca95a59d64266
ThijsJ04 <tijnvdk@gmail.com>
2025-06-10 11:35:40 +0200

    feat: Add post-processing and evaluation phases

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from dataset.prompt_loader import load_input_prompts
 from experiment_configuration import experiment_configuration
 from evaluation.evaluator import Evaluator
 from generation.generator import Generator
+from generation.post_processing import apply_post_processing
 
-
-# Specify which phases of the experiment to run
+#####
+## Specify which phases of the experiment to run
+#####
 CODE_GENERATION_PHASE = False
-CODE_UNIT_TESTING_PHASE = True
-CODE_SECURITY_EVALUATION_PHASE = False
+CODE_GENERATION_PHASE_APPLY_POST_PROCESSING = False
+CODE_EVALUATION_PHASE = False
+RESULT_PARSING_PHASE = False
 
 
 if __name__ == "__main__":
     # Change STDOUT and STDERR to /logs
     makedirs("logs", exist_ok=True)
     sys.stderr = open(os_path.join("logs", "stderr.log"), "w")
-    sys.stdout = open(
-        os_path.join("logs", "stdout.log"), "w"
-    )  # Uncomment if you want to redirect stdout as well
+    sys.stdout = open(os_path.join("logs", "stdout.log"), "w")
 
     parser = ArgumentParser(
         description="Generate code using a language model and evaluate it on security and energy efficiency."
@@ -36,16 +37,28 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if CODE_GENERATION_PHASE:
-        print("Starting code generation phase...")
+        print("Starting code generation phase...", file=sys.stdout)
         input_prompts = load_input_prompts(dataset_path=args.dataset)
         generator = Generator(experiment_configuration=experiment_configuration)
         generator.generate(prompts=input_prompts)
+        if CODE_GENERATION_PHASE_APPLY_POST_PROCESSING:
+            print("Applying post-processing to generated code...", file=sys.stdout)
+            apply_post_processing(dataset_path=args.dataset)
+        else:
+            print("Skipping post-processing of generated code.", file=sys.stdout)
     else:
-        print("Skipping code generation phase.")
+        print("Skipping code generation phase.", file=sys.stdout)
 
-    if CODE_UNIT_TESTING_PHASE:
-        print("Starting code unit testing phase...")
+    if CODE_EVALUATION_PHASE:
+        print("Starting code evaluation phase...", file=sys.stdout)
         evaluator = Evaluator()
-        evaluator.run_all_tests()
+        evaluator.run_all_test_based_tests()
+        evaluator.run_static_based_tests()
     else:
-        print("Skipping code unit testing phase.")
+        print("Skipping code evaluation phase.", file=sys.stdout)
+
+    if RESULT_PARSING_PHASE:
+        print("Starting result parsing phase...", file=sys.stdout)
+
+    else:
+        print("Skipping result parsing phase.", file=sys.stdout)
